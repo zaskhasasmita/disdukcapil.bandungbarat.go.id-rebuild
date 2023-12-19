c95918c00333f6ad4a4c4fd377d618ffd91409ee
zaskhasasmita <zaskhasasmita22@gmail.com>
2023-12-19 12:26:51 +0700

    Rebuild Web Disdukcapil - Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary' : '#023047',
+        'secondary' : '#8ecae6',
+        'dark' : '#334155',
+        'orange-z' : '#fb8500',
+        'blue-z' : '#219ebc',
+        'yellow-z' : '#ffb703',
+      }
+    },
+    fontFamily: {
+      'inter' : ['Inter', 'sans-serif'],
+      'montserrat' : ['Montserrat', 'sans-serif']
+    },
   },
   plugins: [],
 }
